1ffafbeb93a7f2e4581b2b90c85b71788d130584
Yuji Ueki <yueki@oneteam.co.jp>
2017-02-09 16:28:28 +0900

    Create empty view

---
+++ lib/src/main/kotlin/io/one_team/oneteam_rte_kt/RichTextEditorView.kt
@@ -0,0 +1,15 @@
+package io.one_team.oneteam_rte_kt
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class RichTextEditorView(context: Context, attr: AttributeSet?) : LinearLayout(context, attr) {
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+    }
+}
