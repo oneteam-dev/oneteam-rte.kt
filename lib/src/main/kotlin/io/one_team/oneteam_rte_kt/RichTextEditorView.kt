f76440e69b9a2689cba5afef63caa7b5f4e1bed6
Yuji Ueki <unhappychoice@gmail.com>
2017-02-10 13:36:53 +0900

    Add webview

--- lib/src/main/kotlin/io/one_team/oneteam_rte_kt/RichTextEditorView.kt
+++ lib/src/main/kotlin/io/one_team/oneteam_rte_kt/RichTextEditorView.kt
@@ -2,14 +2,25 @@ package io.one_team.oneteam_rte_kt
 
 import android.content.Context
 import android.util.AttributeSet
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
 import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.rich_text_editor_view.view.*
 
 class RichTextEditorView(context: Context, attr: AttributeSet?) : LinearLayout(context, attr) {
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
+        webView.loadUrl("file:///android_asset/index.html")
+        webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(JSInterface(), "AndroidInterface")
+
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
     }
+}
+
+class JSInterface {
+    @JavascriptInterface fun debug(): Unit {}
 }
