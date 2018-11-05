ba7ae6eeaa26fd2419e5b2aa434ea2611111aad0
Mariko <maripiyok@gmail.com>
2018-11-05 09:36:57 +0900

    Add sample button for checking debug log of rte editor context.

--- sample/src/main/kotlin/io/one_team/oneteam_rte_kt/sample/MainActivity.kt
+++ sample/src/main/kotlin/io/one_team/oneteam_rte_kt/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package io.one_team.oneteam_rte_kt.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import io.one_team.oneteam_rte_kt.core.Mentionable
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
@@ -30,5 +31,9 @@ class MainActivity : AppCompatActivity() {
             Mentionable(1, "testuser1", "Test User 1", "testuser1@example.com", "http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon"),
             Mentionable(2, "testuser2", "Test User 2", "testuser2@example.com", "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=retro&f=y")
         )
+
+        contentLogButton.setOnClickListener {
+            Log.d("DEBUG", editor.content)
+        }
     }
 }
