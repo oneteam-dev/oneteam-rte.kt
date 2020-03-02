f5d16154f59ca77ed83c679ddf3f0438e67de244
Mariko <maripiyok@gmail.com>
2020-03-03 08:54:52 +0900

    androidx

--- toolbar/src/main/kotlin/io/one_team/oneteam_rte_kt/toolbar/Toolbar.kt
+++ toolbar/src/main/kotlin/io/one_team/oneteam_rte_kt/toolbar/Toolbar.kt
@@ -1,7 +1,6 @@
 package io.one_team.oneteam_rte_kt.toolbar
 
 import android.content.Context
-import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.ImageButton
